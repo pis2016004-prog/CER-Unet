bf3bfcc727cc0f8fbfc9e7b56fa2ff95b0f330c1
Anonymous <your-id+your-username@users.noreply.github.com>
2025-12-10 22:35:57 -0600

    Add CER-UNet BraTS2017 implementation (cernet)

---
+++ cernet/experiment_planning/alternative_experiment_planning/experiment_planner_baseline_3DUNet_v21_3convperstage.py
@@ -0,0 +1,30 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+
+
+from copy import deepcopy
+
+import numpy as np
+from icenet.experiment_planning.common_utils import get_pool_and_conv_props
+from icenet.experiment_planning.experiment_planner_baseline_3DUNet import ExperimentPlanner
+from icenet.experiment_planning.experiment_planner_baseline_3DUNet_v21 import ExperimentPlanner3D_v21
+from icenet.network_architecture.generic_UNet import Generic_UNet
+from icenet.paths import *
+
+
+class ExperimentPlanner3D_v21_3cps(ExperimentPlanner3D_v21):
+    """
+    have 3x conv-in-lrelu per resolution instead of 2 while remaining in the same memory budget
+
+    This only works with 3d fullres because we use the same data as ExperimentPlanner3D_v21. Lowres would require to
+    rerun preprocesing (different patch size = different 3d lowres target spacing)
+    """
+    def __init__(self, folder_with_cropped_data, preprocessed_output_folder):
+        super(ExperimentPlanner3D_v21_3cps, self).__init__(folder_with_cropped_data, preprocessed_output_folder)
+        self.plans_fname = join(self.preprocessed_output_folder,
+                                "nnFormerPlansv2.1_3cps_plans_3D.pkl")
+        self.unet_base_num_features = 32
+        self.conv_per_stage = 3
+
+    def run_preprocessing(self, num_threads):
+        pass
