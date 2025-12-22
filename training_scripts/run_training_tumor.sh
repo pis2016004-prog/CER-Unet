c44559cc3ef109668d23315a28ec9d9757c6ebde
anonymous <pis2016004@iiita.ac.in>
2025-12-21 23:21:33 -0600

    Update run_training_tumor.sh

--- training_scripts/run_training_tumor.sh
+++ training_scripts/run_training_tumor.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=../output_tumor
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task03_tumor
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
-python ../unetr_pp/run/run_training.py 3d_fullres unetr_pp_trainer_tumor 3 0
+python cernet/run/run_training.py 3d_fullres cernet_trainer_tumor 3 0
