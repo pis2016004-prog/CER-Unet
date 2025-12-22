d5a7bf9a1d280c58088c73a4e35c5a2db3150049
anonymous <pis2016004@iiita.ac.in>
2025-12-21 23:20:20 -0600

    Update run_training_synapse.sh

--- training_scripts/run_training_synapse.sh
+++ training_scripts/run_training_synapse.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=./output_synapse_INCE2
 export unetr_pp_preprocessed=../DATASET/DATASET_Synapse/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
-python icenet/run/run_training.py 3d_fullres icenet_trainer_synapse 2 0 
+python cernet/run/run_training.py 3d_fullres cernet_trainer_synapse 2 0 
